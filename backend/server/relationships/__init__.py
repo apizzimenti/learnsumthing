488f00f5f0daa6fa58c93d2d08e4196a7f69cee4
apizzimenti <pizzimentianthony@gmail.com>
2016-12-24 18:54:14 -0500

    validate Cursor object ids

--- backend/server/relationships/__init__.py
+++ backend/server/relationships/__init__.py
@@ -2,3 +2,15 @@ from .things import *
 from .metadata import *
 from .people import *
 from .interactions import *
+
+
+# function to serialize ObjectIds
+def validate_results(results):
+
+    data = []
+
+    for result in results:
+        result["_id"] = str(result["_id"])
+        data.append(result)
+
+    return data
