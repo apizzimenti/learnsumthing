cdde4d5cb55094e0d625c0aa93d36ae6e6297f8b
apizzimenti <pizzimentianthony@gmail.com>
2016-12-24 18:54:53 -0500

    add students list

--- backend/server/relationships/things/school.py
+++ backend/server/relationships/things/school.py
@@ -2,11 +2,12 @@
 
 class School:
 
-    def __init__(self, name="", location="", instructors=[], subjects=[]):
+    def __init__(self, name="", location="", instructors=[], subjects=[], students=[]):
         self.name = name
         self.location = location
         self.instructors = instructors
         self.subjects = subjects
+        self.students = students
 
     def encode(self):
 
@@ -14,5 +15,6 @@ class School:
             "name": self.name,
             "location": self.location,
             "instructors": [instructor.encode() for instructor in self.instructors],
-            "subjects": [subject.encode() for subject in self.subjects]
+            "subjects": [subject.encode() for subject in self.subjects],
+            "students": [student.encode() for student in self.students]
         }
