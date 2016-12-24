ab06657405324e513c40a156ab1444fe2a1a8346
apizzimenti <pizzimentianthony@gmail.com>
2016-12-24 18:55:35 -0500

    student GET : school GET, POST

--- backend/server/views.py
+++ backend/server/views.py
@@ -1,7 +1,9 @@
 from pyramid.response import Response
 from pyramid.view import view_config
 from server.relationships import Container
-import json
+from server.relationships import validate_results
+from bson.objectid import ObjectId
+from json import loads, dumps
 
 
 class Server:
@@ -19,8 +21,71 @@ class Server:
 
         return Response(
             content_type="application/json",
-            body=json.dumps(c.__dict__)
+            body=dumps(c.__dict__)
         )
 
-    @view_config(route_name="student")
-    def
+    @view_config(route_name="student_GET")
+    def student_GET(self):
+
+        db = self.request.db.test
+        name = self.request.params.get("name", None)
+        objectId = self.request.params.get("_id", None)
+        body = None
+
+        if not name and not objectId:
+            students = db.sumthing.find({"school.students": {}})
+        elif name and not objectId:
+            db.sumthing.find({"school.students.name": name})
+        else:
+            db.sumthing.find({"school.students._id": objectId})
+
+        return Response(
+            content_type="application/json",
+            body=body
+        )
+
+    @view_config(route_name="school_GET")
+    def school_GET(self):
+
+        db = self.request.db.test
+        name = self.request.params.get("name", None)
+        location = self.request.params.get("location", None)
+        objectId = self.request.params.get("_id", None)
+
+        if objectId:
+            result = db.sumthing.find_one({"_id": ObjectId(objectId)})
+            result["_id"] = str(result["_id"])
+
+            re = Container(data=[result])
+
+            return Response(
+                content_type="application/json",
+                body=dumps(re.encode())
+            )
+
+        elif name and not location:
+            result = db.sumthing.find({"name": name})
+        elif name and location:
+            result = db.sumthing.find({"name": name, "location": location})
+        else:
+            result = db.sumthing.find({"location": location})
+
+        re = Container(data=validate_results(result))
+
+        return Response(
+            content_type="application/json",
+            body=dumps(re.encode())
+        )
+
+    @view_config(route_name="school_POST")
+    def school_POST(self):
+
+        db = self.request.db.test
+        req = self.request.json_body
+        db.sumthing.insert_one(req)
+
+        return Response(
+            content_type="application/json",
+            body=self.request.body
+        )
+
